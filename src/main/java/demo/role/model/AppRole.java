019fc96fbb32a41ff60876836830a7344db858e6
Long <longpp@AM.LOCAL>
2019-11-27 13:19:10 +0700

    commit
    Signed-off-by: Long <longpp@AM.LOCAL>

---
+++ src/main/java/demo/role/model/AppRole.java
@@ -0,0 +1,26 @@
+package demo.role.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Data
+@Entity
+@Table(name = "App_Role", uniqueConstraints = {
+                @UniqueConstraint(name = "APP_ROLE_UK", columnNames = "Role_Name") })
+public class AppRole {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "Role_Id", nullable = false)
+    private Long roleId;
+
+    @Column(name = "Role_Name", length = 30, nullable = false)
+    private String roleName;
+
+}
